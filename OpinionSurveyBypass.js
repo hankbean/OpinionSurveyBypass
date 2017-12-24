130c184f07ba9634dc5fe896948d310a79b5bef5
hankbean <hank200702@gmail.com>
2017-12-25 06:43:16 +0800

    不提交的版本

--- OpinionSurveyBypass.js
+++ OpinionSurveyBypass.js
@@ -5,13 +5,42 @@ javascript:(function () {
   var f3 = f2.frames[1];
 
   var text = f3.document.getElementsByTagName("body")[0].innerHTML;
-  var regex = /id.*?(rdBtn.*?Y)"/g;
-  
+
+  var regex = /id.*(rdBtn.*5)" type/g;
   while ((m = regex.exec(text)) !== null ) {
+    console.log(m);
+    if(f3.document.getElementById(m[1]) !== null)
+    f3.document.getElementById(m[1]).checked = true;
+  } 
+
+  var regex = /id.*?(rdBtn.*?Y)" type/g;
+  while ((m = regex.exec(text)) !== null ) {
+    console.log(m);
+    if(f3.document.getElementById(m[1]) !== null)
+    f3.document.getElementById(m[1]).checked = true;
+  } 
+            
+  var regex = /id.*?(chk.*?)"/g;
+  while ((m = regex.exec(text)) !== null ) {
+    console.log(m);
+    if(f3.document.getElementById(m[1]) !== null)
     f3.document.getElementById(m[1]).checked = true;
   }
-  
-  f3.document.getElementById("txtAnsLike").value = "非常棒!";
-  f3.document.getElementById('btnIns').click();
+
+  var regex = /id.*?(gvData.*?5)"/g;
+  while ((m = regex.exec(text)) !== null ) {
+    console.log(m);
+    if(f3.document.getElementById(m[1]) !== null)
+    f3.document.getElementById(m[1]).checked = true;
+  }
+
+  var regex = /id.*?(gvTeaTarget.*?5)"/g;
+  while ((m = regex.exec(text)) !== null ) {
+    console.log(m);
+    if(f3.document.getElementById(m[1]) !== null)
+    f3.document.getElementById(m[1]).checked = true;
+  }
+
+  f3.document.getElementById("txtAnsLike").value = "真的真的非常棒!";
 
 })();
